6ed5070bfe4d84aa824817a73f9c113e02c13260
David Granström <david@davidgranstrom.com>
2019-01-21 22:58:33 +0100

    Run server loop in a separate thread

--- rplugin/python3/scnvim/__init__.py
+++ rplugin/python3/scnvim/__init__.py
@@ -1,6 +1,7 @@
+from threading import Thread
+import json
 import pynvim
 import socket
-import json
 
 ADDR = '127.0.0.1' # loopback
 PORT = 9670
@@ -9,32 +10,25 @@ PORT = 9670
 class SCNvim(object):
     def __init__(self, nvim):
         self.nvim = nvim
+        self.server = None
+        self.vim_leaving = False
         self.server_started = False
 
-    def _echo(self, message):
+    def echo(self, message):
         try:
-            self.nvim.command('echo "{}"'.format(message))
-        except ValueError as e:
-            self.nvim.err_write('[scnvim]: ' + e)
+            self.nvim.out_write(message + '\n')
+        except BaseException as e:
+            self.nvim.err_write('[scnvim]: ' + str(e))
 
-    def _stl_update(self, object):
+    def stl_update(self, object):
         try:
             json_str = json.dumps(object)
-            # self.nvim.async_call('scnvim#statusline#update', [json_str])
             self.nvim.call('scnvim#statusline#update', json_str)
-        except ValueError as e:
-            self.nvim.err_write('[scnvim]: ' + e)
+        except BaseException as e:
+            self.nvim.err_write('[scnvim]: ' + str(e))
 
-    @pynvim.function('__scnvim_server_start', sync=False)
-    def server_start(self, args):
-        if self.server_started:
-            return
-
-        self.server_started = True
-        self.server = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server.bind((ADDR, PORT))
-        while(True):
+    def server_loop(self):
+        while not self.vim_leaving:
             data, addr = self.server.recvfrom(1024)
             data = data.decode('utf-8')
             try:
@@ -42,12 +36,26 @@ class SCNvim(object):
                 status_line = data.get('status_line', '')
                 method_args = data.get('method_args', '')
                 if method_args:
-                    self._echo(method_args)
+                    self.nvim.async_call(self.echo, method_args)
                 if status_line:
-                    self._stl_update(status_line)
-            except ValueError as e:
+                    self.nvim.async_call(self.stl_update, status_line)
+            except BaseException:
                 self.nvim.err_write('[scnvim]: json decode error')
 
-    @pynvim.function('__scnvim_current_dir', sync=True)
-    def get_current_dir(self, args):
-        return self.nvim.eval('expand("%:p")')
+    @pynvim.function('__scnvim_server_start', sync=False)
+    def server_start(self, args):
+        if self.server:
+            return
+        self.server = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind((ADDR, PORT))
+
+        self.thread = Thread(target=self.server_loop)
+        self.thread.start()
+
+    @pynvim.autocmd('VimLeave', pattern='*', sync=True)
+    def on_vim_leave(self):
+        self.vim_leaving = True
+        if self.server:
+            self.server.shutdown()
+            self.server.close()
