30fbaa2d20d8e1c36268f51700bed35a635e2bb6
David Granström <david@davidgranstrom.com>
2020-05-01 13:56:21 +0200

    Add install scripts for mac and linux

---
+++ bin/install_linux.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+dest_dir=$HOME/Library/Application\ Support/SuperCollider/Extensions/scide_scvim
+./install_unix.sh "$dest_dir"
---
+++ bin/install_unix.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+plugin_dir=$(dirname "$(pwd)")
+dest_dir="$1"
+
+# if destination is a symbolic link (old install instructions)
+if [ -L "$dest_dir" ]; then
+  echo "$dest_dir" already exists! Delete it and run this script again.
+fi
+
+mkdir -p "$dest_dir"
+[ ! -e "$dest_dir/scnvim" ] && ln -s "$plugin_dir/sc" "$dest_dir/scnvim"
