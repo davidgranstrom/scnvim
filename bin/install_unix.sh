1bed8acdcb0b083e78207516ea39b8c62bd6b47f
David Granström <david@davidgranstrom.com>
2020-05-01 14:33:09 +0200

    Exit codes

--- bin/install_unix.sh
+++ bin/install_unix.sh
@@ -6,7 +6,12 @@ dest_dir="$1"
 # if destination is a symbolic link (old install instructions)
 if [ -L "$dest_dir" ]; then
   echo "$dest_dir" already exists! Delete it and run this script again.
+  exit 1
 fi
 
 mkdir -p "$dest_dir"
-[ ! -e "$dest_dir/scnvim" ] && ln -s "$plugin_dir/sc" "$dest_dir/scnvim"
+if [ ! -e "$dest_dir/scnvim" ]; then
+  ln -s "$plugin_dir/sc" "$dest_dir/scnvim"
+fi
+
+exit 0
