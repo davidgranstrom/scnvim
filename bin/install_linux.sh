4745238c9237466104d6143335f2fb3f1e100491
David Granström <david@davidgranstrom.com>
2020-05-01 14:06:04 +0200

    Fix linux path

--- bin/install_linux.sh
+++ bin/install_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-dest_dir=$HOME/Library/Application\ Support/SuperCollider/Extensions/scide_scvim
+dest_dir=$HOME/.local/share/SuperCollider/Extensions/scide_scvim
 ./install_unix.sh "$dest_dir"
