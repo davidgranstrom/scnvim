53f74b7103784cdba34f10233a9931acda8d3785
David Granström <david@davidgranstrom.com>
2021-01-02 19:08:54 +0100

    Add some more tests

--- test/spec/sclang_spec.lua
+++ test/spec/sclang_spec.lua
@@ -28,13 +28,39 @@ describe('sclang', function()
       end
     end)
     assert.is_true(result)
+    assert.is_true(sclang.is_running())
+  end)
+
+  it('can receive commands', function()
+    sclang.send('1 + 1', false)
+    vim.cmd('sleep 1')
+    assert.are.equal('-> 2', stdout[#stdout])
+  end)
+
+  it('can pass results to lua callback', function()
+    sclang.eval('7 * 7', function(res)
+      assert.are.equal(49, res)
+    end)
+    vim.cmd('sleep 1')
+  end)
+
+  it('can recompile client', function()
+    sclang.recompile()
+    vim.cmd('sleep 1')
+    local result = 0
+    for _, line in ipairs(stdout) do
+      -- this line should now appear twice in the output
+      if line:match('^*** Welcome to SuperCollider') then
+        result = result + 1
+      end
+    end
+    assert.are.equal(2, result)
   end)
 
   it('can stop client', function()
-    vim.wait(5000, function()
-      sclang.stop()
-      return stdout == nil
-    end)
+    sclang.stop()
+    vim.cmd('sleep 1')
     assert.is_nil(stdout)
+    assert.is_false(sclang.is_running())
   end)
 end)
