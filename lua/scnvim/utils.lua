5b0043b63bc4d970c81b1aefa1c11f7ea3d8a471
mads kjeldgaard <mail@madskjeldgaard.dk>
2021-10-30 11:40:03 +0200

    Luasnip support (#152)
    * feat: Add support for LuaSnip snippets
    
    * Finish luasnip support
    
    * doc: LuaSnip support
    
    * add space between arguments to conform to other snippet engines

--- lua/scnvim/utils.lua
+++ lua/scnvim/utils.lua
@@ -48,7 +48,7 @@ function M.get_snippets()
   local root_dir = M.get_scnvim_root_dir()
   local format = M.get_var('scnvim_snippet_format') or 'snippets.nvim'
   local snippet_dir = root_dir .. M.path_sep .. 'scnvim-data'
-  if format == 'snippets.nvim' then
+  if format == 'snippets.nvim' or format == 'luasnip' then
     local filename = snippet_dir .. M.path_sep .. 'scnvim_snippets.lua'
     local ok, file = pcall(loadfile(filename))
     if ok then
