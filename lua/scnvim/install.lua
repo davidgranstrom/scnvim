cbab1c1bd9c67f222a9f0939586a0667f5f2516c
David Granström <david@davidgranstrom.com>
2021-10-30 13:01:17 +0200

    Add a setup function

--- lua/scnvim/install.lua
+++ lua/scnvim/install.lua
@@ -62,21 +62,17 @@ end
 function M.link()
   local link_target = get_target_dir()
   local target_exists = uv.fs_stat(link_target)
-  -- create the link
   if not target_exists then
     local source = scnvim_root_dir .. utils.path_sep .. 'scide_scnvim'
     assert(uv.fs_symlink(source, link_target, {dir = true, junction = true}))
-    print('[scnvim] Installed to: ' .. link_target)
   end
 end
 
 --- Remove symlink to the SCNvim classes
 function M.unlink()
   local link_target = get_target_dir()
-  -- remove the link
   if is_symlink(link_target) then
     assert(uv.fs_unlink(link_target))
-    print('[scnvim] Uninstalled ' .. link_target)
   end
 end
 
