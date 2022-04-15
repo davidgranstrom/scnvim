3410396daedeecd424def439d4c5af5f6b176b53
ranjithshegde <ranjithshegde@gmail.com>
2022-04-15 15:17:13 +0200

    *.sc, *.scd, *.quark are natively recognized in 0.7
    Integrate with 0.7 release by removing redundant detects

--- ftdetect/filetype.lua
+++ ftdetect/filetype.lua
@@ -1,8 +1,6 @@
 if vim.fn.has "nvim-0.7" == 1 and vim.g.do_filetype_lua == 1 then
   vim.filetype.add {
     extension = {
-      sc = "supercollider",
-      scd = "supercollider",
       schelp = "scdoc",
     },
   }
