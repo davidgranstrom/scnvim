0f8064b5d8647ba10017127386a5b73dff12a169
David Granström <david@davidgranstrom.com>
2022-05-19 13:49:59 +0200

    Add unit test for user commands

---
+++ test/spec/commands_spec.lua
@@ -0,0 +1,29 @@
+local create_commands = require 'scnvim.commands'
+
+describe('commands', function()
+  it('creates user commands for the current buffer', function()
+    vim.cmd [[ edit commands.scd ]]
+    create_commands()
+    local expected = {
+      'SCNvimGenerateAssets',
+      'SCNvimHelp',
+      'SCNvimRecompile',
+      'SCNvimStart',
+      'SCNvimStatusLine',
+      'SCNvimStop',
+      'SCNvimTags', -- deprecated
+    }
+    local cmds = vim.api.nvim_buf_get_commands(0, {})
+    assert.are.equal(#expected, vim.tbl_count(cmds))
+    local count = 0
+    for _, key in ipairs(expected) do
+      local c = cmds[key]
+      if c ~= nil then
+        count = count + 1
+      end
+    end
+    assert.are.equal(#expected, count)
+    vim.cmd [[ bd! ]]
+  end)
+end)
+
