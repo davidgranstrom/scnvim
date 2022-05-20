8f18685f1d4c45ce4c50f4586326a4c871647ad2
David Granström <david@davidgranstrom.com>
2022-05-20 13:01:21 +0200

    Add more cases to editor unit test

--- test/spec/automated/editor_spec.lua
+++ test/spec/automated/editor_spec.lua
@@ -1,6 +1,5 @@
 local editor = require 'scnvim.editor'
 
-local buf
 local content = [[
 x = 123;
 (
@@ -9,11 +8,22 @@ x * 2;
 )
 x = 7;
 x = x * 2;
+(
+var x = 7;
+// )
+x * 2;
+)
+(
+var y = 3;
+y * 2;
+)foo
 ]]
-content = vim.split(content, '\n', { plain = true, trimempty = true })
-buf = vim.api.nvim_create_buf(false, true)
-vim.api.nvim_buf_set_lines(buf, -2, -1, false, content)
+
+local buf = vim.api.nvim_create_buf(false, true)
 vim.api.nvim_win_set_buf(0, buf)
+vim.api.nvim_buf_set_option(buf, 'filetype', 'supercollider')
+content = vim.split(content, '\n', { plain = true, trimempty = true })
+vim.api.nvim_buf_set_lines(buf, -2, -1, false, content)
 
 describe('editor', function()
   before_each(function()
@@ -30,7 +40,6 @@ describe('editor', function()
   it('can send a code block', function()
     vim.api.nvim_win_set_cursor(0, { 2, 0 })
     editor.send_block(function(data)
-      -- print(vim.inspect(data))
       local block = table.concat(data, '\n')
       local expected = [[
 (
@@ -42,7 +51,7 @@ x * 2;
     end)
   end)
 
-  it('can send a visual selection', function()
+  it('can send linewise visual selection', function()
     vim.api.nvim_win_set_cursor(0, { 6, 0 })
     vim.cmd [[normal! V]]
     vim.api.nvim_win_set_cursor(0, { 7, 0 })
@@ -52,6 +61,47 @@ x * 2;
 x = 7;
 x = x * 2;]]
       assert.are.equal(expected, selection)
+      return data
+    end)
+  end)
+
+  it('can send a visual selection', function()
+    vim.api.nvim_win_set_cursor(0, { 6, 0 })
+    vim.cmd [[normal! v]]
+    vim.api.nvim_win_set_cursor(0, { 6, 4 })
+    editor.send_selection(function(data)
+      local selection = table.concat(data, '\n')
+      local expected = 'x = 7'
+      assert.are.equal(expected, selection)
+      return data
+    end)
+  end)
+
+  it('ignores parenthesis in comments', function()
+    vim.api.nvim_win_set_cursor(0, { 8, 0 })
+    editor.send_block(function(data)
+      local block = table.concat(data, '\n')
+      local expected = [[
+(
+var x = 7;
+// )
+x * 2;
+)]]
+      assert.are.equal(expected, block)
+      return data
+    end)
+  end)
+
+  it('ignores everything after block end', function()
+    vim.api.nvim_win_set_cursor(0, { 13, 0 })
+    editor.send_block(function(data)
+      local block = table.concat(data, '\n')
+      local expected = [[
+(
+var y = 3;
+y * 2;
+)
+)]]
       return data
     end)
   end)
