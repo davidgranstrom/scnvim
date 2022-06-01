18139e89a9fc1993dc7acb82b103e32ecb33f4ba
David Granström <david@davidgranstrom.com>
2022-06-01 16:36:49 +0200

    Improve health report

--- lua/scnvim/health.lua
+++ lua/scnvim/health.lua
@@ -15,7 +15,8 @@ local function check_nvim_version()
     health.report_error 'scnvim needs nvim version 0.7 or higher.'
     health.report_info 'if you are unable to upgrade, use the `0.6-compat` branch'
   else
-    health.report_ok 'nvim version is 0.7 or higher.'
+    local v = vim.version()
+    health.report_ok(string.format('nvim version %d.%d.%d', v.major, v.minor, v.patch))
   end
 end
 
@@ -39,9 +40,9 @@ end
 
 local function check_documentation()
   local doc = config.documentation
-  if not doc then
+  if not doc.cmd then
     health.report_info 'using HelpBrowser for documentation'
-  elseif doc.cmd then
+  else
     local exe_path = vim.fn.exepath(doc.cmd)
     if exe_path ~= '' then
       health.report_ok(doc.cmd)
@@ -58,16 +59,19 @@ local function check_documentation()
       end
       if vin and vout then
         health.report_ok(vim.inspect(doc.args))
-      elseif vin and not vout then
-        health.report_error 'argument list is missing input placeholder ($1)'
       elseif vout and not vin then
+        health.report_error 'argument list is missing input placeholder ($1)'
+      elseif vin and not vout then
         health.report_error 'argument list is missing output placeholder ($2)'
+      else
+        health.report_error 'argument list is missing both input and output placeholders ($1/$2)'
       end
-    else
-      health.report_error('no argument list found for ' .. doc.cmd)
     end
-    if doc.selector then
-      health.report_info 'using external selector for methods'
+    if doc.on_open then
+      health.report_info 'using external function for on_open'
+    end
+    if doc.on_select then
+      health.report_info 'using external function for on_select'
     end
   end
 end
