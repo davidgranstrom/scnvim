cce3ec84fd7942a5edd1ec2e73fcfa22f1bc3b28
David Granström <david@davidgranstrom.com>
2022-06-20 17:43:51 +0200

    Override path.root_dir on CI

--- test/spec/automated/install_spec.lua
+++ test/spec/automated/install_spec.lua
@@ -1,5 +1,10 @@
 local install = require 'scnvim.install'
 
+local is_ci = vim.loop.os_getenv 'SCNVIM_CI'
+if is_ci then
+  require('scnvim.path').root_dir = vim.fn.expand '%:p:h:h'
+end
+
 describe('install', function()
   it('can link scnvim classes', function()
     install.install()
